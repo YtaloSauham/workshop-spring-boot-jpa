69b04f38a65ca1699b11cc785b566d2c0d276967
YtaloSauham <ytalo.sauhan@hotmail.com>
2022-10-22 17:35:27 -0300

    Project created

---
+++ src/main/java/com/backend/apirestbackendspringboot/ApirestBackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.backend.apirestbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestBackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestBackendSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/apirestbackendspringboot/ApirestBackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.apirestbackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestBackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
