1637a5f8033025e3bb99510f181f9e6ae92d6067
YtaloSauham <ytalo.sauhan@hotmail.com>
2022-10-24 18:42:21 -0300

    test config createad

---
+++ src/main/java/com/backend/apirestbackendspringboot/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.backend.apirestbackendspringboot.repositories;
+
+import com.backend.apirestbackendspringboot.entities.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users,Long> {
+
+}
